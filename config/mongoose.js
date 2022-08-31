9aba09c0da6a12e82965eb2a37d9b2575ea56c6f
sagar-kpr <102784500+sagar-kpr@users.noreply.github.com>
2022-08-31 11:34:44 +0530

    initialize social_media app

---
+++ config/mongoose.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/social_development');
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console,"error while connecting db"));
+
+db.once('open', () => {
+    console.log('database conected');
+})
