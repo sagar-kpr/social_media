f3f58950cc0145380073ac61c83489cc78f3f513
sagar-kpr <sagarkpr6@gmail.com>
2022-09-05 19:41:20 +0530

    particular comment delete of posts

--- controller/comment_controller.js
+++ controller/comment_controller.js
@@ -13,11 +13,27 @@ module.exports.create = function(req,res){
                     
                     post.comment.push(comments);
                     post.save();
-                    
                     res.redirect('/profile');
                 }
             );
 
         }
     });
+}
+
+module.exports.destroy = function(req,res){
+    Comment.findById(req.params.id, function(err,comment){
+        if(comment.user == req.user.id){
+            let postId = comment.post;
+            comment.remove();
+            Post.findByIdAndUpdate(postId, { $pull : {comment: req.params.id}}, function(err){
+                return res.redirect('back');
+            });
+        }else{
+            return res.redirect('back');
+        }
+    });
+
+    
+
 }
