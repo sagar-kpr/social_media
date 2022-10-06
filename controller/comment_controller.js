2e9d830f9ce1f1b3844c91dcef211c9e56ca8d1c
sagar-kpr <sagarkpr6@gmail.com>
2022-10-07 01:11:09 +0530

    added kue

--- controller/comment_controller.js
+++ controller/comment_controller.js
@@ -1,11 +1,12 @@
 const Post = require('../models/post_schema');
 const Comment = require('../models/comment_schema');
 const commentMailer = require('../mailers/comment_mailer');
+const queue = require('../config/kue');
+const commentWorker = require('../worker/comment_worker');
 
 module.exports.create = async function(req,res){
     try{
         let post = await Post.findById(req.body.post);
-        console.log('+++',req.body);
         
         if(post){
             let comments = await Comment.create(
@@ -17,10 +18,16 @@ module.exports.create = async function(req,res){
             );
             post.comment.push(comments);
             post.save();
-            comments  = await comments.populate('user', 'email');
-            console.log('this++++', comments.user.email);
-            commentMailer.newComment(comments);
-            
+            comments  = await comments.populate('user','email');
+            let job = queue.create('emails', comments).save(function(err){
+                if(err){
+                    console.log('err in creating queue', err);
+                    return;
+                }
+                console.log('queue created :', job.id );
+                
+            });
+            //commentMailer.newComment(comments);
 
             if(req.xhr){
                 return res.status(200).json({
@@ -42,6 +49,41 @@ module.exports.create = async function(req,res){
         console.log('error', err);
     }
 }
+
+// module.exports.create = function(req,res){
+//     Post.findById(req.body.post,function(err,post){
+//         if(post){
+//             Comment.create({
+//                 content: req.body.content,
+//                 post: req.body.post,
+//                 user: req.user._id
+//             },function(err, comment){
+//                 if(err){
+//                     // console.log('error in adding comments');
+//                     req.flash('error',err);
+//                     return;
+//                 }
+//                 post.comment.push(comment);
+//                 post.save();
+//                 comment.populate('user', 'name email');
+//                 // commentsMailer.newComment(comment)// we are calling newComment
+
+//                 let job = queue.create('emails',comment).save(function(err){ // comment I am going to send in the email
+//                     if(err){
+//                         console.log('error in sending to the queue',err);
+//                         return;
+//                     }
+
+//                     console.log('job enqueued',job.id);
+//                 });
+//                 req.flash('success','Comment created!')
+
+//                 res.redirect('/');
+//             });
+//         }
+//     });
+// }
+
 
 
 module.exports.destroy = async function(req,res){
--- mailers/comment_mailer.js
+++ mailers/comment_mailer.js
@@ -1,12 +1,12 @@
 const nodemailer = require('../config/nodemailer');
 
 exports.newComment = (comments) => {
+    htmlString = nodemailer.renderTemplate({comment : comments}, '/comment/new_comment.ejs');
     nodemailer.transporter.sendMail({
-        
         from : 'sagarkpr6@gmail.com',
         to: comments.user.email,
         subject: 'mail sent',
-        html: '<h1>first email</h1>'
+        html: htmlString
     }, (err, info) => {
         if(err) { console.log('err in sending email:::', err); return}
         console.log('msg done', info);
