a457d5f35a03366558585d59e438b088137309f5
sagar-kpr <sagarkpr6@gmail.com>
2022-10-11 00:33:24 +0530

    shift to main

---
+++ models/like_schema.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const likeSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref : 'User'
+    },
+    likeable : {
+        type: String,
+        required : true,
+        refPath : 'onModel'
+    },
+    onModel : {
+        type: String,
+        required: true,
+        enum:['Post', 'Comment']
+    }
+
+},{
+    timestamps: true
+});
+
+
+const Like = mongoose.model('Like', likeSchema);
+
+module.exports = Like;
