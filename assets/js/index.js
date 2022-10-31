a3051b14bc4888b4e8e705871bbb06565b6080d1
accounts8 <accounts8@kwalitypharma.co.in>
2022-10-31 18:03:40 +0530

    ui change

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,15 +5,6 @@ var k = 0;
 var arrow2 = document.getElementById('arrow2');
 var i = 0;
 
-<<<<<<< HEAD
-/*var mini = document.getElementById('minimize');
-var m = 0;
-
-var cross = document.getElementById('cross');
-var l= 0;*/
-=======
-
->>>>>>> d71c97b56139069c54ba6dbbee53bdb9e3a8e34d
 
 arrow1.addEventListener('click', function(){
     if(k % 2 == 0){
@@ -33,27 +24,4 @@ arrow2.addEventListener('click', function(){
     i++;
 });
 
-<<<<<<< HEAD
-/*cross.addEventListener('click', function(){
-    if(l % 2 == 0){
-        document.getElementById('chat-box').style.display = 'none';
-    }
-    
-})
-
-mini.addEventListener('click', function(){
-    if(m % 2 == 0){
-        console.log('hi')
-        document.getElementById('chat-box').style.height = '15px';
-        document.getElementById('chat-container').style.overflow = 'hidden';
-    }
-    else{
-        document.getElementById('chat-box').style.height = '380px';
-    }
-    m++;
-
-})*/
-=======
-
->>>>>>> d71c97b56139069c54ba6dbbee53bdb9e3a8e34d
 
