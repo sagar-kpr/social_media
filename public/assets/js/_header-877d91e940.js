9f673a4c6e7d8e61d9d4302977751b6f23de62e5
sagar-kpr <sagarkpr6@gmail.com>
2022-11-04 20:08:14 +0530

    some

--- public/assets/js/_header-877d91e940.js
+++
@@ -1,1 +0,0 @@
-const messenger=document.getElementById("messenger");var minimize=document.getElementById("minimize"),i=0;messenger.addEventListener("click",(function(){document.getElementById("msg-box").style.display="block"}));
