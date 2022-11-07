5577698f9d04c3504aca8fda84045a6dc7a61e05
sagar-kpr <sagarkpr6@gmail.com>
2022-11-07 23:21:33 +0530

    port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 require('./config/view_helper')(app);
 const logger = require('morgan');
 const db = require('./config/mongoose');
-const port = 8000 //process.env.PORT || 8000  ;
+const port = process.env.PORT || 8000  ;
 const parser = require('body-parser');
 const layout = require('express-ejs-layouts');
 const passport = require('passport');
